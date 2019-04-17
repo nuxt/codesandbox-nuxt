13a4b944ab84ae3961e3f07d4d5690ee81047e32
Sébastien Chopin <seb@chopin.io>
2019-04-17 16:17:44 +0200

    chore: Use @nuxt/http, add comments for docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,55 +1,57 @@
-import pkg from './package.json'
-
 export default {
+  /*
+  ** Rendering mode
+  ** Doc: https://nuxtjs.org/api/configuration-mode
+  */
   mode: 'universal',
 
   /*
   ** Headers of the page
+  ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
   */
   head: {
-    title: pkg.name,
+    title: 'Nuxt.js starter for CSB',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Official Nuxt.js starter for CodeSandBox' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#3b8070' },
-
-  /*
   ** Global CSS
+  ** Doc: https://nuxtjs.org/api/configuration-css
   */
   css: [],
 
   /*
   ** Plugins to load before mounting the App
+  ** Doc: https://nuxtjs.org/guide/plugins
   */
   plugins: [],
 
   /*
   ** Nuxt.js modules
+  ** Doc: https://nuxtjs.org/guide/modules
   */
   modules: [
-    // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios',
+    // Doc: https://http.nuxtjs.org
+    '@nuxt/http',
     // TODO: Remove it if you want to eject from codeSandbox
     './codesandbox'
   ],
 
   /*
-  ** Axios module configuration
+  ** HTTP module configuration
   */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
+  http: {
+    // See https://http.nuxtjs.org/api/#options
   },
 
   /*
   ** Build configuration
+  ** Doc: https://nuxtjs.org/api/configuration-build
   */
   build: {
     /*
