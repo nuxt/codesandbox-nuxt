f7c94ceb3c70b8a2ed4eecba90b35497f9703d11
KunJoha <37103381+KunJoha@users.noreply.github.com>
2020-02-12 09:24:20 +0100

    grundgeruest

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,101 @@
 <template>
-  <div class="container">
-    <Nuxt/>
+  <div>
+    <div id="logo">
+      <img src="~/assets/Logo-gross.png">
+    </div>
+    <div id="navbar">
+      <NLink to="/" class="button--orange">Home</NLink>
+      <NLink to="/leistung" class="button--orange">Leistung</NLink>
+      <NLink to="/referenzen" class="button--orange">Referenzen</NLink>
+      <NLink to="/preise" class="button--orange">Preise</NLink>
+    </div>
+    <div id="content-sidebar-merge">
+      <div id="page-content">
+        <Nuxt/>
+      </div>
+      <div id="always-sidebar">
+        <aside id="sidebar-box">
+          <h3>Kontakt</h3>
+          <img src="~/assets/Logo-kontakt.png">
+          <p>André Zießnitz
+            <br>Kuf steiner Straße 20
+            <br>14612 Falkensee
+          </p>
+          <p>Telefon: 03322 | 289 283</p>
+          <p>Handy: 0173 | 89 65 423</p>
+          <p>Telefax: 03322 | 289 282</p>
+          <p>E-Mail: info@Hot-Systems.de</p>
+        </aside>
+      </div>
+    </div>
+    <footer>
+      <p>Inhaber: André Zießnitz</p>
+      <p>UStNr.: 051|292|04275 Finanzamt Nauen</p>
+      <p>Technischer Verantwortlicher: André Zießnitz</p>
+      <p>Inhaltlich Verantwortlicher gemäß</p>
+      <p>§ 6 MDStV: André Zießnitz</p>
+      <p>AGBs</p>
+    </footer>
   </div>
 </template>
-
+ 
 <style>
+#content-sidebar-merge {
+  background-color: white;
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1000px;
+}
+
+#page-content {
+  flex: 5;
+}
+
+#always-sidebar {
+  flex: 2;
+}
+
+#sidebar-box {
+  margin: 10px;
+  height: 100%;
+  border: 1px solid lightgray;more
+  padding: 10px;
+}
+
+#logo {
+  background-color: orange;
+}
+
+#logo > img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 900px;
+  width: 100%;
+  padding: 20px;
+}
+
+#navbar {
+  background-color: orangered;
+  padding: 10px 0px;
+  height: 50px;
+  text-align: center;
+}
+
+footer {
+  padding-top: 30px;
+  text-align: center;
+  background-color: black;
+}
+
+body {
+  background-color: lightgray;
+}
+
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -24,29 +112,35 @@ html {
   box-sizing: border-box;
   margin: 0;
 }
+.container_text {
+  display: float;
+  justify-content: center;
+  align-items: center;
+  text-align: left;
+}
 
 .container {
-  min-height: 100vh;
+  min-height: 250px;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
 
-.button--green {
+.button--orange {
   display: inline-block;
   border-radius: 4px;
-  border: 1px solid #00C48D;
-  color: #00C48D;
+  border: 1px solid rgb(209, 118, 14);
+  color: rgb(209, 118, 14);
   text-decoration: none;
-  padding: 10px 30px;
+  padding: 5px 15px;
   display: inline-flex;
   align-self: center;
 }
 
-.button--green:hover {
+.button--orange:hover {
   color: #fff;
-  background-color: #00C48D;
+  background-color: rgb(209, 118, 14);
 }
 
 .button--grey {
--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,61 @@
 <template>
   <section>
-    <div>
-      <Logo :width="350"/>
-      <h1 class="title">NUXT<span class="green">JS</span></h1>
-      <h2 class="subtitle">Starter for CodeSandBox</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation <IconLink/></a>
-        <NLink to="/about" class="button--grey">About</NLink>
+    <section class="content">
+      <h1>PC Reparatur in Falkensee</h1>
+      <br>
+      <div id="intro">
+        <p>HOTSYSTEMS Ihr Notdienst</p>
+        <p>in Falkensee, Berlin und Umgebung</p>
       </div>
-    </div>
+      <br>
+      <div id="short-discription">
+        <p>Bei uns bekommen Sie kompetente Beratung, alle Installations- und Wartungsleistungen rund um Ihre PC-Anlage aus einer Hand. Profitieren Sie von unserem Service schnell - günstig - zuverlässig und zu jeder Tageszeit!</p>
+        <p>Von der privat genutzten Anlage bis hin zu Firmenlösungen für bis zu 500 Clients. Hardware, Software, Netzwerke, Internetanschluss, Sicherheitskomponenten, Webdesign</p>
+      </div>
+      <br>
+      <br>
+      <div id="need-help">Sie benötigen Hilfe
+        <ul>
+          <li>bei der Datensicherung? Daten sind weg oder sollen vor dem Zugriff Unbefugter geschützt werden?</li>
+          <li>weil Ihre PC-Anlage nicht mehr hochfährt oder eine Fehlermeldung gibt?</li>
+          <li>weil die Internetverbindung nicht mehr funktioniert oder immer langsamer wird?</li>
+          <li>bei der Planung eines Netzwerkes?</li>
+          <li>bei der Installation eines Druckers?</li>
+          <li>weil sich möglicherweise ein Virus auf Ihrer Anlage befindet?</li>
+          <li>bei der Auswahl und beim Kauf von Hard- und Software - zugeschnitten auf Ihren Bedarf?</li>
+        </ul>
+      </div>
+    </section>
   </section>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import IconLink from '~/components/IconLink.vue'
-
-export default {
-  components: {
-    Logo,
-    IconLink
-  }
-}
-</script>
-
 <style scoped>
+.content {
+  width: 700px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 400;
   font-size: 100px;
-  color: #2E495E;
+  color: rgb(201, 73, 34);
   letter-spacing: 1px;
   font-size: 6em;
 }
 .green {
-  color: #00C48D;
+  color: rgb(214, 31, 18);
 }
 
 .subtitle {
   font-weight: 300;
   font-size: 3em;
-  color: #2E495E;
+  color: #ee7621;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
