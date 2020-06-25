6e9262e56289d7e6af157b324d0af7cbb186dfa0
Sascha Dobschal <s.dobschal@tdsoftware.de>
2020-06-25 10:09:28 +0200

    clean up

--- util/jsonResponse.js
+++
@@ -1,5 +0,0 @@
-export const jsonResponse = function(res, data, status = 200) {
-  res.statusCode = status;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data, null, 3));
-};
