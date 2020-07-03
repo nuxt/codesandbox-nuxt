027efeee5503ab2f63630ebca537e78a60573880
Sascha Dobschal <s.dobschal@tdsoftware.de>
2020-07-03 13:13:06 +0200

    create dir if not exist

--- api/coupons.js
+++ api/coupons.js
@@ -3,6 +3,26 @@ import fs from "fs";
 import path from "path";
 
 export default function(req, res, next) {
+  if (!fs.existsSync(path.resolve(__dirname, "../data/"))) {
+    fs.mkdirSync(path.resolve(__dirname, "../data/"));
+  }
+
+  if (!fs.existsSync(path.resolve(__dirname, "../data/coupons.json"))) {
+    fs.writeFileSync(
+      path.resolve(__dirname, "../data/coupons.json"),
+      "[]",
+      "utf8"
+    );
+  }
+
+  if (!fs.existsSync(path.resolve(__dirname, "../data/chart.json"))) {
+    fs.writeFileSync(
+      path.resolve(__dirname, "../data/chart.json"),
+      "{}",
+      "utf8"
+    );
+  }
+
   const data = fs.readFileSync(
     path.resolve(__dirname, "../data/coupons.json"),
     "utf8"
