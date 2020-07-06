a2fb13bf53466d8f60fd225f53fe92bedacec70b
Sascha Dobschal <s.dobschal@tdsoftware.de>
2020-07-06 12:21:23 +0200

    test

--- middleware/hostCheck.js
+++ middleware/hostCheck.js
@@ -1,5 +1,3 @@
 export default function({ store, redirect, isServer, res, req }) {
-  if (process.env.NODE_ENV !== "development") {
-    console.log("Request: ", req.headers.referer);
-  }
+  console.log("Request: ", JSON.stringify(req));
 }
