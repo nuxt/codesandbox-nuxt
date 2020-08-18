d58cfb743a8197203daaffa6c6a60e191c51c285
Bastian Kemmer <b.kemmer@tdsoftware.de>
2020-08-18 11:58:12 +0200

    add dynamic route

--- api/legal/getLegalTexts.js
+++ api/legal/getLegalTexts.js
@@ -2,7 +2,13 @@ import {jsonResponse} from "../../util/jsonResponse";
 import filesystem from "../service/filesystem";
 
 export default async function (req, res, next) {
+   const filter = req.query.filter;
    const legalTexts = await filesystem.getAll("legal");
+   if(filter) {
+      return jsonResponse(res, {
+         legal: legalTexts.find(l => l.id === filter)
+      });
+   }
    jsonResponse(res, {
       legal: legalTexts
    });
