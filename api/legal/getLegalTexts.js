ef735f316300b404b2dcb0a5f72ca1271a39dce7
bastiankemmer <b.kemmer@tdsoftware.de>
2020-08-18 17:26:36 +0200

    fix

--- api/legal/getLegalTexts.js
+++ api/legal/getLegalTexts.js
@@ -1,15 +1,22 @@
-import {jsonResponse} from "../../util/jsonResponse";
+import { jsonResponse } from "../../util/jsonResponse";
 import filesystem from "../service/filesystem";
+import url from "url";
 
 export default async function (req, res, next) {
-   const filter = req.query.filter;
-   const legalTexts = await filesystem.getAll("legal");
-   if(filter) {
+  try {
+    let { filter } = url.parse(req.url, true).query;
+    const legalTexts = await filesystem.getAll("legal");
+    if (filter) {
       return jsonResponse(res, {
-         legal: legalTexts.find(l => l.id === filter)
+        legal: legalTexts.find((l) => l.id === filter)
       });
-   }
-   jsonResponse(res, {
+    }
+    jsonResponse(res, {
       legal: legalTexts
-   });
+    });
+  } catch (e) {
+    jsonResponse(res, {
+      legal: []
+    });
+  }
 }
