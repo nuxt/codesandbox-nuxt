3ee9c86d863b4ed84db2a7839bf266fe4677f57b
Sébastien Chopin <seb@nuxtjs.com>
2021-02-22 15:52:44 +0100

    chore: improvements

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,56 +1,37 @@
 export default {
   /*
-  ** Headers of the page
-  ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
-  */
+   ** Headers of the page
+   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
+   */
   head: {
-    title: 'Nuxt.js starter for CSB',
+    title: "Nuxt.js starter for CSB",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Official Nuxt.js starter for CodeSandBox' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Official Nuxt.js starter for CodeSandBox"
+      }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   /*
-  ** Global CSS
-  ** Doc: https://nuxtjs.org/api/configuration-css
-  */
+   ** Nuxt.js modules
+   ** Doc: https://modules.nuxtjs.org
+   */
+  modules: [],
+
+  /*
+   ** Global CSS
+   ** Doc: https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-css
+   */
   css: [],
 
   /*
-  ** Plugins to load before mounting the App
-  ** Doc: https://nuxtjs.org/guide/plugins
-  */
-  plugins: [],
-
-  /*
-  ** Nuxt.js modules
-  ** Doc: https://nuxtjs.org/guide/modules
-  */
-  modules: [
-    // Doc: https://http.nuxtjs.org
-    '@nuxt/http',
-    // TODO: Remove it if you want to eject from codeSandbox
-    './codesandbox'
-  ],
-
-  /*
-  ** HTTP module configuration
-  */
-  http: {
-    // See https://http.nuxtjs.org/api/#options
-  },
-
-  /*
-  ** Build configuration
-  ** Doc: https://nuxtjs.org/api/configuration-build
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {}
-  }
+   ** Plugins to load before mounting the App
+   ** Doc: https://nuxtjs.org/docs/2.x/directory-structure/plugins
+   */
+  plugins: []
 };
