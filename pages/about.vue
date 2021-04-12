6e1c4a17cc6e1bd9cda5668cf2fb85095e6051d6
Olzhas <81673062+olzhastyle@users.noreply.github.com>
2021-04-12 13:51:57 +0600

    initial commit

--- pages/about.vue
+++ pages/about.vue
@@ -1,21 +1,10 @@
 <template>
   <div class="container">
-    <div>
-      <h1>Hello from <span class="name">{{ name }}</span>.</h1>
-      <p><NLink to="/" class="button--grey">Back home</NLink></p>
-    </div>
+    <div></div>
   </div>
 </template>
 
-<script>
-export default {
-  asyncData() {
-    return {
-      name: (process.server ? 'server' : 'client')
-    }
-  }
-}
-</script>
+
 
 <style scoped>
 .container {
---
+++ pages/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <div></div>
+  </section>
+</template>
+
+
+<style scoped>
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 400;
+  font-size: 100px;
+  color: #2E495E;
+  letter-spacing: 1px;
+  font-size: 6em;
+}
+.green {
+  color: #00C48D;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 3em;
+  color: #2E495E;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
