8426aeaa22fbf993391354154ad20731c2039332
Javier Granda Ruiz <jgrandar@users.noreply.github.com>
2021-05-13 00:37:58 +0200

    initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,5 @@
 <template>
-  <section>
-    <div>
-      <Logo :width="350"/>
-      <h1 class="title">NUXT<span class="green">JS</span></h1>
-      <h2 class="subtitle">Starter for CodeSandBox</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation <IconLink/></a>
-        <NLink to="/about" class="button--grey">About</NLink>
-      </div>
-    </div>
-  </section>
+  
 </template>
 
 <script>
---
+++ pages/productos/_slug/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NuxtLink
+      :to="{ name: '-empresa', params: { empresa: marca.empresa.slug } }"
+      v-text="marca.empresa.nombreComercial"
+    />
+  </div>
+</template>
