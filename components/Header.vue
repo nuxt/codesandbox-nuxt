298948291fce49f084229a9576cb5946aaafca36
akaimar <39114801+akaimar@users.noreply.github.com>
2022-05-18 16:04:31 +0300

    initial commit

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex-container-header">
+    <div style="flex-grow: 1">Laenuhaldur</div>
+    <div style="flex-grow: 8"><a href="?">Meist | Blog | API</a></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pealkiri: "Lanuinfo",
+    };
+  },
+};
+</script>
+
+<style>
+.flex-container-header {
+  display: flex;
+  align-items: stretch;
+  background-color: #f1f1f1;
+}
+.flex-container-header > div {
+  background-color: DodgerBlue;
+  color: white;
+  margin: 10px;
+  line-height: 75px;
+  font-size: 20px;
+}
+</style>
---
+++ components/LoanData.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="flex-container-main">
+    <div v-for="value in loan_data" :key="value.id">
+      <div>Laenu id: {{ value.id }}</div>
+      <div>Summa: {{ parseFloat(value.amount).toLocaleString("et") }}</div>
+      <div>Esimene makse: {{ value.graafik.payment1.total }}</div>
+      <div>---</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loan_data: [
+        {
+          id: "00001",
+          amount: "100000",
+          graafik: {
+            payment1: {
+              total: "100",
+              interest: "10",
+              principal: "90",
+              residual: "900",
+            },
+          },
+        },
+        {
+          id: "00002",
+          amount: "200000",
+          graafik: {
+            payment1: {
+              total: "200",
+              interest: "30",
+              principal: "750",
+              residual: "800",
+            },
+          },
+        },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+.flex-container-main {
+  background-color: #f1f1f1;
+}
+.flex-container-main > div {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  background-color: lightgray;
+  margin: 10px;
+  text-align: left;
+  line-height: 20px;
+  font-size: 16px;
+}
+</style>
